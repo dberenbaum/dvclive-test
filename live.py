9d4fea6ea1813e29d1989ef87b184606034021fa
dberenbaum <dave@iterative.ai>
2023-06-30 17:21:47 -0400

    init

---
+++ live.py
@@ -0,0 +1,9 @@
+import random
+
+from dvclive import Live
+
+live = Live(save_dvc_exp=True)
+for i in range(10):
+    live.log_metric("train/metric", random.random())
+    live.log_metric("test/metric", random.random())
+live.end()
