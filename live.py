405ec3f2096e73eedd1c3c927cdb0ed3918f9a21
dberenbaum <dave@iterative.ai>
2023-06-30 17:24:14 -0400

    add next_step

--- live.py
+++ live.py
@@ -6,4 +6,5 @@ live = Live(save_dvc_exp=True)
 for i in range(10):
     live.log_metric("train/metric", random.random())
     live.log_metric("test/metric", random.random())
+    live.next_step()
 live.end()
