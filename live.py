92af87e720bfc466e766840ded8c78524d7109fb
dberenbaum <dave@iterative.ai>
2023-12-20 07:35:34 -0500

    dvc stage

--- live.py
+++ live.py
@@ -1,10 +1,12 @@
 import random
+import numpy as np
 
 from dvclive import Live
 
-live = Live(save_dvc_exp=True)
-for i in range(10):
-    live.log_metric("train/metric", random.random())
-    live.log_metric("test/metric", random.random())
-    live.next_step()
-live.end()
+with Live() as live:
+    for i in range(10):
+        live.log_metric("train/metric", random.random())
+        live.log_metric("test/metric", random.random())
+        img_numpy = np.random.randint(0, 254, (500, 500), dtype=np.uint8)
+        live.log_image("numpy.png", img_numpy)
+        live.next_step()
