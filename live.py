b1c5d3112c8b9ea067795503346427eb79620d59
dberenbaum <dave@iterative.ai>
2023-12-20 07:42:47 -0500

    dvc: commit experiment 6810901328921cb315cf05cb1195708e52a0062ca59f791ae4d59b74f91d467f

--- live.py
+++ live.py
@@ -4,7 +4,7 @@ import numpy as np
 from dvclive import Live
 
 with Live() as live:
-    for i in range(10):
+    for i in range(20):
         live.log_metric("train/metric", random.random())
         live.log_metric("test/metric", random.random())
         img_numpy = np.random.randint(0, 254, (500, 500), dtype=np.uint8)
